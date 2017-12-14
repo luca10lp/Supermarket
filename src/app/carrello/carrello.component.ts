dce40fea2b0d4c565b757b4f76713a19cb78ca2f
luca10lp <luca.mastroianni96@libero.it>
2017-12-14 15:38:36 +0100

    carrello 1.1

---
+++ src/app/carrello/carrello.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ProdottoComponent} from "../prodotto/prodotto.component";
+import {ProdottoService} from "../../provider/prodotto.service";
+import {Router} from "@angular/router";
+import {SharedService} from "../../provider/shared.service";
+
+@Component({
+  selector: 'app-carrello',
+  templateUrl: './carrello.component.html',
+  styleUrls: ['./carrello.component.css']
+})
+export class CarrelloComponent implements OnInit {
+
+
+  prodCompo: ProdottoComponent;
+
+  constructor(private prodottoService: ProdottoService,  private router: Router,  private _sharedService: SharedService) { }
+
+  ngOnInit() {
+    this.prodCompo.listaCarrello;
+  }
+
+}
